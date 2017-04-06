a299dbafb12ff294fea3c8e6bb2402661305ecfa
daniel lizik <dg.lizik@gmail.com>
2017-04-06 09:57:40 -0400

    tesf

--- src/components/policies/validator.js
+++ src/components/policies/validator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import queryPolicies from '~/src/components/policies/queryPolicies'
 import mutationPolicies from '~/src/components/policies/mutationPolicies'
 
-export default (props) => (props.type === 'QUERY' ? queryPolicies : mutationPolicies).reduce((acc, policy, i, {length}) => {
+export default (props) => ({QUERY: queryPolicies, MUTATION: mutationPolicies})[props.type].reduce((acc, policy, i, {length}) => {
   const isValidTarget = (policy.target || []).includes(props.clause.target)
   const isValidRule = (policy.rules || []).includes(props.clause.rule)
   const Structure = policy.structure
